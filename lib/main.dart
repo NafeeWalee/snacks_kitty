9577f901af568c3af8b371825b0dceb803210743
Nafeeul Alam Walee <nafeewalee@gmail.com>
2021-01-20 17:58:00 +0600

    firebase iOS integrated

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:pure_live_chat/Utils/controller/sizeConfig.dart';
 import 'package:pure_live_chat/src/Screens/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await Firebase.initializeApp();
   runApp(InitiateApp());
 }
 
@@ -25,13 +27,15 @@ class InitiateApp extends StatelessWidget {
       theme: ThemeData(
           visualDensity: VisualDensity.adaptivePlatformDensity,
           scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'PermanentMarker-Regular',
           appBarTheme: AppBarTheme(
               elevation: 0,
               color: Colors.deepPurple,
               centerTitle: false,
               iconTheme: IconThemeData(
                   color: Colors.black
-              )
+              ),
+
           )
       ),
       home: HomePage(),
